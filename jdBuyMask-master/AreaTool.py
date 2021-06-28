cce9b1c488f0f41ed99419dd54f0ef0c49bee132
yao0013 <myname@mymail.comyao0013@126.com>
2021-06-28 23:15:06 +0800

    本地化部署

--- jdBuyMask-master/AreaTool.py
+++ jdBuyMask-master/AreaTool.py
@@ -4,7 +4,7 @@ from jdlogger import logger
 '''
 找一条第一个版本的url
 '''
-url = 'https://c0.3.cn/stock?skuId=100003406321&area=19_1607_4773_0&venderId=1000000946&buyNum=1&choseSuitSkuIds=&cat=9192,9197,12588&extraParam={%22originid%22:%221%22}&fqsp=0&pdpin=jd_7c3992aa27d1a&pduid=1580535906442142991701&ch=1&callback=jQuery4291064'
+url = 'https://p.3.cn/prices/mgets?callback=jQuery7046778&type=1&area=19_1655_39461_0&pdtk=&pduid=277273792&pdpin=yao0013&pin=yao0013&pdbp=0&skuIds=J_100011513445%2CJ_100017153370%2CJ_1980618455%2CJ_41181339933%2CJ_47731058821%2CJ_69522895216%2CJ_28027806194%2CJ_100010343850%2CJ_100001484839%2CJ_5968860%2CJ_63488181381%2CJ_100007131001%2CJ_40357736144%2CJ_44638775908&ext=11100000&source=item-pc'
 skuId = url.split('skuId=')[1].split('&')[0]
 area = url.split('area=')[1].split('&')[0]
 logger.info('你的area是[ %s ]，链接的商品id是[ %s ]', area, skuId)
